2697dbb24c8ec5a5bcb63f001763a6e9680501e3
GuoGuo <gch981213@gmail.com>
2015-06-28 19:17:03 +0800

    添加Runtime Error判断，修改时间输出

--- judge.sh
+++ judge.sh
@@ -10,27 +10,32 @@ fi
 echo "[0m"
 chmod +x p$1.outprg
 TOTTIME=0
-#MEMLIMIT=$(curl http://110.90.118.124/OnlineJudge/problem_show.php?id=$1 2>/dev/null |\
+#MEMLIMIT=$(curl http://172.0.11.5/OnlineJudge/problem_show.php?id=$1 2>/dev/null |\
 #    grep '内存限制' |\
 #    awk '{print $3}' |\
 #    awk -F '：' '{print $2}' |\
 #    awk -F 'K' '{print $1}')
 for i in \
-    $(curl http://110.90.118.124/OnlineJudge/Data/$1/config.ini 2>/dev/null | sed '1d')
+    $(curl http://172.0.11.5/OnlineJudge/Data/$1/config.ini 2>/dev/null | sed '1d')
 do
     INPUTFILE=$(echo $i | awk -F '|' '{print $1}')
     OUTFILE=$(echo $i | awk -F '|' '{print $2}')
     TIMELIMIT=$(echo $i | awk -F '|' '{print $3}')
     echo "====[33mDownloading Data[0m====="
-    curl http://110.90.118.124/OnlineJudge/Data/$1/$INPUTFILE > data.in
-    curl http://110.90.118.124/OnlineJudge/Data/$1/$OUTFILE >data.out
+    curl http://172.0.11.5/OnlineJudge/Data/$1/$INPUTFILE > data.in
+    curl http://172.0.11.5/OnlineJudge/Data/$1/$OUTFILE >data.out
     echo =========================
-    rm mydata.out
-    ./ujudger-run $(echo $TIMELIMIT|awk '{print (int($1)==$1?$1:int(int($1*10/10+1)))*5}') ./p$1.outprg data.in mydata.out > res
-#    /usr/bin/time -f "%U %M" -o used_time ./p$1.outprg < data.in >mydata.out
+    rm mydata.out 2> /dev/null
+    ./ujudger-run $(echo $TIMELIMIT|awk '{print (int($1)==$1?$1:int(int($1*10/10+1)))*2}') ./p$1.outprg data.in mydata.out > res
+    EXITSTAT=$(cat res | awk '{print $6}')
     USEDTIME=$(cat res | awk '{print $3}')
     USEDMEM=$(cat res | awk '{print $4}')
     echo Tested $INPUTFILE
+    if [ $EXITSTAT -eq 11 ]
+    then
+            ANSRES="[31mRuntime Error[0m"
+            echo $ANSRES
+    else
     if diff -uNZ data.out mydata.out > /dev/null
     then
         echo Answer Correct.
@@ -51,19 +56,16 @@ do
     echo Time limit:$TIMELIMIT s
 #    echo Memory limit:$MEMLIMIT KB
     echo =========================
+    fi
     if [ $(echo $USEDTIME $TIMELIMIT | awk '{if($1>$2){print "1"}else {print "0"}}') = 1 ]
     then
         TIMERES="[31mTime Limit Exceed."
     else
-        TIMERES="[32mTime Check Passed."
+        TIMERES=$(printf "[32mTime:%11f" $USEDTIME)
     fi
-#    if [ $USEDMEM -ge $MEMLIMIT ]
-#    then
-MEMRES=$(printf "[33mMemory:%11dKB" $USEDMEM)
-#    else
-#        MEMRES="[32mMemory Check Passed."
-#    fi
-    echo "$INPUTFILE	$ANSRES	$TIMERES	$MEMRES[0m" >> result
+    [ $EXITSTAT -eq 0 ] || TIMERES=""
+    MEMRES=$(printf "[33mMemory:%11dKB" $USEDMEM)
+    echo "$INPUTFILE	$ANSRES	$TIMERES   $MEMRES[0m" >> result
 done
 cat result
 echo
