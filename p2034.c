bc891abcdca16dbd351f73119bec03064ed4d79c
GuoGuo <gch981213@gmail.com>
2015-06-28 21:38:00 +0800

    20150628

---
+++ p2034.c
@@ -0,0 +1,1 @@
+int a,b,c=1,d;int main(){scanf("%d",&a);getchar();for(;a;a--){while((b=getchar())!='\n')c=((d=!d)?(b=='l'?c*2:c*2+2):(b=='l'?c*2-1:c*2+1))%1000000007;printf("%d\n",c);c=!(d=0);}return 0;}
---
+++ p2106.c
@@ -0,0 +1,1 @@
+unsigned int a,b,c;int main(){scanf("%d",&a);while(a--){scanf("%*u%u",&b);c+=b;}return !printf("%u",c);}
